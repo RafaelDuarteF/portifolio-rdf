c54c21a4f6538f5e08d8cb7439316215f44349f5
Rafael Duarte <rafa.duartef@outlook.com>
2024-09-29 19:17:19 -0300

    First commit

---
+++ src/constants/config.ts
@@ -0,0 +1,96 @@
+type TSection = {
+  p: string;
+  h2: string;
+  content?: string;
+};
+
+type TConfig = {
+  html: {
+    title: string;
+    fullName: string;
+    email: string;
+  };
+  hero: {
+    name: string;
+    p: string[];
+  };
+  contact: {
+    form: {
+      name: {
+        span: string;
+        placeholder: string;
+      };
+      email: {
+        span: string;
+        placeholder: string;
+      };
+      message: {
+        span: string;
+        placeholder: string;
+      };
+    };
+  } & TSection;
+  sections: {
+    about: Required<TSection>;
+    experience: TSection;
+    feedbacks: TSection;
+    works: Required<TSection>;
+  };
+};
+
+export const config: TConfig = {
+  html: {
+    title: "Rafael Duarte - Portfólio",
+    fullName: "Rafael Duarte de Freitas",
+    email: "rafa.duartef@outlook.com",
+  },
+  hero: {
+    name: "Rafael Duarte",
+    p: [
+      "Sou um desenvolvedor fullstack web e mobile, especialista",
+      "em Laravel PHP, MySQL, React e React Native.",
+    ],
+  },
+  contact: {
+    p: "Entre em contato",
+    h2: "Contato.",
+    form: {
+      name: {
+        span: "Rafael Duarte",
+        placeholder: "Qual é o seu nome?",
+      },
+      email: { span: "Seu Email", placeholder: "Qual é o seu email?" },
+      message: {
+        span: "Sua Mensagem",
+        placeholder: "O que você gostaria de dizer?",
+      },
+    },
+  },
+  sections: {
+    about: {
+      p: "Introdução",
+      h2: "Visão Geral.",
+      content: `Sou um desenvolvedor de software mobile & web fullstack experiente em
+      JavaScript, PHP e bancos de dados como SQLServer, MySql entre outros. Com especialização em React JS, React Native, Laravel PHP e MySql. Sou um aprendiz rápido e colaboro de perto com os clientes para
+      criar soluções eficientes, escaláveis e amigáveis que resolvem
+      problemas do mundo real. Vamos trabalhar juntos para trazer suas ideias à vida!`,
+    },
+    experience: {
+      p: "O que já fiz até agora",
+      h2: "Experiência de Trabalho.",
+    },
+    feedbacks: {
+      p: "O que outros dizem",
+      h2: "Depoimentos.",
+    },
+    works: {
+      p: "Meu trabalho",
+      h2: "Projetos.",
+      content: `Os projetos a seguir mostram algumas das minhas habilidades e experiência através de
+      exemplos práticos do meu trabalho. Cada projeto é descrito brevemente com
+      links para visualização. Eles refletem minha
+      capacidade de resolver problemas complexos, trabalhar com diferentes tecnologias
+      e gerenciar projetos de forma eficaz.`,
+    },
+  },
+};
---
+++ src/constants/index.ts
@@ -0,0 +1,212 @@
+import type {
+  TNavLink,
+  TService,
+  TTechnology,
+  TExperience,
+  TTestimonial,
+  TProject,
+} from "../types";
+
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks: TNavLink[] = [
+  {
+    id: "about",
+    title: "Sobre",
+  },
+  {
+    id: "work",
+    title: "Trabalhos",
+  },
+  {
+    id: "contact",
+    title: "Contato",
+  },
+];
+
+const services: TService[] = [
+  {
+    title: "Desenvolvedor Web",
+    icon: web,
+  },
+  {
+    title: "Desenvolvedor React Native",
+    icon: mobile,
+  },
+  {
+    title: "Desenvolvedor Backend",
+    icon: backend,
+  },
+  {
+    title: "Bancos de Dados Relacionais",
+    icon: mongodb,
+  },
+];
+
+const technologies: TTechnology[] = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Bancos de dados",
+    icon: mongodb,
+  },
+  {
+    name: "Laravel PHP",
+    icon: backend,
+  },
+  {
+    name: "Git",
+    icon: git,
+  },
+];
+
+const experiences: TExperience[] = [
+  {
+    title: "Lead Developer Fullstack",
+    companyName: "BizVision Tech",
+    icon: meta,
+    iconBg: "#252525",
+    date: "Agosto 2024 - Atualmente",
+    points: [
+      "Desenvolver e manter aplicativos web usando React.js",
+      "Desenvolver e manter APIs RESTful usando Laravel PHP com Lúmen, e conexões com bancos de dados diversos",
+      "Desenvolver e manter aplicativos mobile usando React Native",
+      "Colaborar com equipes multifuncionais, incluindo designers, gerentes de produto e outros desenvolvedores, para criar produtos de alta qualidade.",
+      "Implementar design responsivo e garantir a compatibilidade entre navegadores.",
+      "Implementar mecanismos de segurança diversos, como proteção a ataques DDos, bots e vazamento de dados.",
+    ],
+  },
+  {
+    title: "Lead Developer Fullstack",
+    companyName: "Soares Consulting",
+    icon: tesla,
+    iconBg: "#252525",
+    date: "Jan 2024 - Atualmente",
+    points: [
+      "Desenvolver e manter aplicativos web usando React.js",
+      "Desenvolver e manter APIs RESTful usando Laravel PHP com Lúmen, e conexões com bancos de dados diversos",
+      "Desenvolver e manter aplicativos mobile usando React Native",
+      "Colaborar com equipes multifuncionais, incluindo designers, gerentes de produto e outros desenvolvedores, para criar produtos de alta qualidade.",
+      "Implementar design responsivo e garantir a compatibilidade entre navegadores.",
+      "Implementar mecanismos de segurança diversos, como proteção a ataques DDos, bots e vazamento de dados.",
+    ],
+  },
+];
+
+const testimonials: TTestimonial[] = [
+  {
+    testimonial:
+      "Depois que o Rafael entrou na empresa, deixamos de ser apenas consultoria e nos tornamos uma software house. Ele é um desenvolvedor incrível, mas além disso, um solucionador de problemas nato.",
+    name: "Yago Soares",
+    designation: "CEO",
+    company: "Soares Consulting",
+    image: "https://media.licdn.com/dms/image/v2/D4D03AQE7gVBI460org/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1678372039827?e=1733356800&v=beta&t=H3SoTHkuC0Zkfep9VYvOFnCqCoB6gOKnESI5_ZtEKwk",
+  },
+];
+
+const projects: TProject[] = [
+  {
+    name: "SCTech Embedded",
+    description:
+      "Plataforma baseada na web que permite a empresas organizarem a visualização de seus dashboards em Power BI dentro da plataforma, para seus usuários finais. A plataforma é personalizada para cada cliente, permitindo a adição de logotipos, cores e temas personalizados.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mysql",
+        color: "green-text-gradient",
+      },
+      {
+        name: "laravel",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "powerbi-api",
+        color: "green-text-gradient",
+      },
+    ],
+    image: carrent,
+    sourceCodeLink: "https://portaldataholds.com/",
+  },
+  {
+    name: "BizVision Tech",
+    description:
+      "Website para informatização da empresa BizVision, com o objetivo de apresentar seus serviços e produtos, além de permitir a solicitação de orçamentos e contato direto com a empresa.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    sourceCodeLink: "https://bizvisiontech.com/",
+  },
+  {
+    name: "ISP4pp",
+    description:
+      "Aplicativo mobile para visualização de dados de provedores de internet, com o objetivo de facilitar a visualização de dados de clientes, contratos e financeiro (principais KPI's). Além da visualização de alertas críticos da companhia.",
+    tags: [
+      {
+        name: "reactnative",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "php",
+        color: "green-text-gradient",
+      },
+      {
+        name: "styled-components",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    sourceCodeLink: "",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
